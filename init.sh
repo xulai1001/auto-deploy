92d9167a37caa5af458a9d68337fb429076549b1
islab <islab@whu.edu>
2016-03-29 13:42:55 +0800

    update init.sh to ruby 2.0

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-apt-get install ruby rake 
+apt-get install ruby2.0 rake 
+gem sources --add https://ruby.taobao.org/ --remove https://rubygems.org
+gem sources -l
 gem install term-ansicolor require_all
 
