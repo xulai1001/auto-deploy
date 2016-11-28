f8b0e92d0f013bc4e49f8c5516b80deebd5889ce
Viktor Xu <xulai1001@gmail.com>
2016-11-28 16:34:36 +0800

    fix ubuntu.sh

--- bootstrap/ubuntu.sh
+++ bootstrap/ubuntu.sh
@@ -9,10 +9,10 @@ fi
 set_repo()
 {
     url="mirrors.aliyun.com"
-    if [ $1 = "16.04" ]; then
+    if [ $1 = '"16.04"' ]; then
         ver="xenial"
     else
-        if [ $1 = "14.04" ]; then ver="trusty"; fi
+        if [ $1 = '"14.04"' ]; then ver="trusty"; fi
     fi
     echo "设置ubuntu软件源..." $1 $ver
     wget http://raw.githubusercontent.com/xulai1001/auto-deploy/master1/bootstrap/ubuntu-template.repo -O ubuntu.repo
